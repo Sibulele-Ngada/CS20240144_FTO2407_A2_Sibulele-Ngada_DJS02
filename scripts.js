19bb2b1314cddebeaccbc3f62bff51abc58a0d94
Sibulele Ngada <sibulelengada@gmail.com>
2024-10-26 13:41:32 +0200

    User story - crash program when input NaN

--- scripts.js
+++ scripts.js
@@ -5,12 +5,16 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  console.log(divider);
-  if (!dividend || !divider) {
+  if (isNaN(dividend) || isNaN(divider)) {
+    document.querySelector(
+      "body"
+    ).innerHTML = `<h1>Something critical went wrong. Please reload the page</h1>`;
+    console.error(`Something critical went wrong. Please reload the page`);
+  } else if (!dividend || !divider) {
     result.innerText = `Division not performed. Both values are required in inputs. Try again`;
   } else if (divider == 0) {
-    console.error(`Division not performed. Invalid number provided. Try again`);
     result.innerText = `Division not performed. Invalid number provided. Try again`;
+    console.error(`Division not performed. Invalid number provided. Try again`);
   } else {
     result.innerText = Math.floor(dividend / divider);
   }
