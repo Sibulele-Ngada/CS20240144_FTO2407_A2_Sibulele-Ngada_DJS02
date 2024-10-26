5fd290ac50a42f92bc9a4722ad8581f0e5028ca2
Sibulele Ngada <sibulelengada@gmail.com>
2024-10-26 13:26:44 +0200

    User story error protection when dividing by 0

--- scripts.js
+++ scripts.js
@@ -5,9 +5,13 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
+  console.log(divider);
   if (!dividend || !divider) {
     result.innerText = `Division not performed. Both values are required in inputs. Try again`;
-    return;
+  } else if (divider == 0) {
+    console.error(`Division not performed. Invalid number provided. Try again`);
+    result.innerText = `Division not performed. Invalid number provided. Try again`;
+  } else {
+    result.innerText = Math.floor(dividend / divider);
   }
-  result.innerText = Math.floor(dividend / divider);
 });
