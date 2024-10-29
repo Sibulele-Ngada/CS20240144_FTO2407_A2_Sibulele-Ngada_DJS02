c65211b55608471fc2ee8333ee9d92e68a144604
Sibulele Ngada <sibulelengada@gmail.com>
2024-10-29 14:20:19 +0200

    Refactored after validation. Please mark from previous commit

--- scripts.js
+++ scripts.js
@@ -5,17 +5,33 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  if (isNaN(dividend) || isNaN(divider)) {
-    document.querySelector(
-      "body"
-    ).innerHTML = `<h1>Something critical went wrong. Please reload the page</h1>`;
-    console.error(`Something critical went wrong. Please reload the page`);
-  } else if (!dividend || !divider) {
-    result.innerText = `Division not performed. Both values are required in inputs. Try again`;
-  } else if (divider == 0) {
-    result.innerText = `Division not performed. Invalid number provided. Try again`;
-    console.error(`Division not performed. Invalid number provided. Try again`);
-  } else {
+
+  try {
+    // Validate inputs not empty
+    if (!dividend || !divider) {
+      result.innerText = `Division not performed. Both values are required in inputs. Try again`;
+      throw new Error(
+        `Division not performed. Both values are required in inputs. Try again`
+      );
+    }
+    // Validate dividing by 0
+    if (divider == 0) {
+      result.innerText = `Division not performed. Invalid number provided. Try again`;
+      throw new Error(
+        `Division not performed. Invalid number provided. Try again`
+      );
+    }
+    // Validate input NaN
+    if (isNaN(dividend) || isNaN(divider)) {
+      document.querySelector(
+        "body"
+      ).innerHTML = `<h1>Something critical went wrong. Please reload the page</h1>`;
+      throw new Error(`Something critical went wrong. Please reload the page`);
+    }
+
+    // Display result without decimals
     result.innerText = Math.floor(dividend / divider);
+  } catch (err) {
+    console.error(err);
   }
 });
